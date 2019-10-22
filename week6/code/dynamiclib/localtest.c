06d11bc9d0fc22bcf57c0ffbe3fc64ba80165caa
sxp <1553284789@qq.com>
2019-10-22 04:49:26 -0700

    Add week6

---
+++ week6/code/dynamiclib/localtest.c
@@ -0,0 +1,11 @@
+#include "dylib.h"
+#include <stdio.h>
+int main()
+{
+	int a[UNUMBER];
+	init(a,UNUMBER);
+	show(a,UNUMBER);
+	
+	printf("sum=%d,max=%d\n",sum(a,UNUMBER),max(a,UNUMBER));
+	return 0;
+}
